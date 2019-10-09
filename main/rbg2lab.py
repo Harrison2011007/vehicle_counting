ca3db81ee8f36261539ec6b1aee21aa30fff9eb9
Harrison2011007 <352770725@qq.com>
2019-10-09 08:33:03 +0800

    图片全部像素转换成功

--- main/rbg2lab.py
+++ main/rbg2lab.py
@@ -1,21 +1,55 @@
 from PIL import Image
 
-
+# 读取图片并获取每个像素的rgb值
 imTree = Image.open('E:\\work2019\\Mar\\images\\car.jpg')
 pix = imTree.load()
-Twidth = imTree.size[0]
-Theight = imTree.size[1]
-print(Twidth)
-imTree = imTree.convert('RGB')
+Twidth = imTree.size[0]#获取图片宽度
+Theight = imTree.size[1]#获取图片长度
+# print(Twidth)
+imTree = imTree.convert('RGB')#获取rgb值
+#建立一个序列，将rgb值存入其中
 rgbarray = []
-
 for x in range(Twidth):
     for y in range(Theight):
         r,g,b = imTree.getpixel((x,y))
         rgb = r,g,b
         rgbarray.append(rgb)
+# 打印试一下对不对
+print(rgbarray)
 print(rgbarray[0])
 print(rgbarray[0][0])
+print(rgbarray[0][1])
+print(rgbarray[0][2])
+print(len(rgbarray))
+# lab值的接收值
 l = []
-l = 0.2126007 * rgbarray[0][0] + 0.7151947 * rgbarray[0][1] + 0.0722046 * rgbarray[0][2]
+a = []
+b = []
+# rgb到lab转换公式如下：
+# L = 0.2126007 * R + 0.7151947 * G + 0.0722046 * B;
+# a = 0.3258962 * R - 0.4992596 * G + 0.1733409 * B + 128;
+# b = 0.1218128 * R + 0.3785610 * G - 0.5003738 * B + 128;
+for i in range(len(rgbarray)):
+    l1 = int(0.2126007 * rgbarray[i][0] + 0.7151947 * rgbarray[i][1] + 0.0722046 * rgbarray[i][2])#l1,a1,b1是过渡值
+    a1 = int(0.3258962 * rgbarray[i][0]  - 0.4992596 * rgbarray[i][1] + 0.1733409 * rgbarray[i][2] + 128)
+    b1 = int(0.1218128 * rgbarray[i][0] + 0.3785610 * rgbarray[i][1] - 0.5003738 * rgbarray[i][2] + 128)
+    l.append(l1)
+    b.append(b1)
+    a.append(a1)
+# 试一下第一个值转化对不对
+# print(l)
+# l = 0.2126007 * rgbarray[0][0] + 0.7151947 * rgbarray[0][1] + 0.0722046 * rgbarray[0][2]
+# a = 0.3258962 * rgbarray[0][0]  - 0.4992596 * rgbarray[0][1] + 0.1733409 * rgbarray[0][2] + 128
+# b = 0.1218128 * rgbarray[0][0] + 0.3785610 * rgbarray[0][1] - 0.5003738 * rgbarray[0][2] + 128
 print(l)
+print(a)
+print(b)
+lab = []
+for i in range(len(l)):
+    lab1 = l[i],a[i],b[i]#lab1是过渡值
+    lab.append(lab1)
+# 打印出lab值
+print(lab)#rgb到lab值转换完毕
+#
+
+
