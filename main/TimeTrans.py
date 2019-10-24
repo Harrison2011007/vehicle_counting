9884f15d3b9dbffb38220145f25fef3e5e0d2c4f
孙浩 <352770725@qq.com>
2019-10-24 21:51:39 +0800

    读取某一行

--- main/TimeTrans.py
+++ main/TimeTrans.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 
 # print(time.time())
-io = r'E:\work2019\出租车数据\Taxi_GPS_2019_09_03.csv'
+io = r'J:\org_xy.csv'
 data = pd.read_csv(io)
 
-print(data.iloc[1:,2])
+print(data['x'].values)
 
